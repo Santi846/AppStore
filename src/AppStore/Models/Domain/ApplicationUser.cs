a7da94609e1bf5f7ed9f07f80be4775f0c320676
Santiago Arocha <arochas846@gmail.com>
2024-06-06 07:05:02 -0300

    Add DB Context: Models

---
+++ src/AppStore/Models/Domain/ApplicationUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AppStore.Models.Domain;
+
+public class ApplicationUser : IdentityUser{
+    public string? Nombre {get;set;}
+};
--- src/AppStore/Models/Domain/DatabaseContext.cs
+++ src/AppStore/Models/Domain/DatabaseContext.cs
@@ -1,5 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace AppStore.Models.Domain;
 
-public class DatabaseContext{
+public class DatabaseContext : IdentityDbContext<ApplicationUser>{
+
+    public DbSet<Categoria>? Categorias {get;set;}
+
+    public DbSet<Libro>? Libros {get;set;}
+
+    public DbSet<LibroCategoria>? LibroCategorias {get;set;}
     
-}
+};
