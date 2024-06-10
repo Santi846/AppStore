cfcbe814c16b3e1397bb04bea83209a58b204540
Santiago Arocha <arochas846@gmail.com>
2024-06-10 07:08:26 -0300

    Add Project: DBContext

--- src/AppStore/Program.cs
+++ src/AppStore/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// builder.Services.AddDbContext<DatabaseContext>(options => options.LogTo(Console.WriteLine, new []){}.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
